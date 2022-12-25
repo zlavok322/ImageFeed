be794b3bbf9ce447718000c48e9940ed7418f6c1
zlavok322 <zarazazaza2010@gmail.com>
2022-12-25 18:13:34 +0300

    init

---
+++ ImageFeed/Helpers/UIColor+Extensions.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+extension UIColor {
+    static var ypBlack: UIColor { UIColor(named: "YPBlack")! }
+    static var ypBackground: UIColor { UIColor(named: "YPBackground")! }
+    static var ypBlue: UIColor { UIColor(named: "YPBlue")! }
+    static var ypGray: UIColor { UIColor(named: "YPGray")! }
+    static var ypRed: UIColor { UIColor(named: "YPRed")! }
+    static var ypWhite: UIColor { UIColor(named: "YPWhite")! }
+    static var ypWhite50: UIColor { UIColor(named: "YPWhite(Alpha50)")! }
+}
